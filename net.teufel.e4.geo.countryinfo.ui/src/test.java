9c0ad91f3852309a2b99ed7ee936ed0652007704
Marc Teufel <marc.teufel@hama.de>
2011-01-05 07:10:33 +0100

    Added a new plugin with a fragment.xml into Workbench Model

---
+++ net.teufel.e4.geo.countryinfo.ui/src/test.java
@@ -0,0 +1,10 @@
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class test {
+	@Execute
+	public void execute() {
+		//TODO Your code goes here
+		System.out.println("test");
+	}
+		
+}
