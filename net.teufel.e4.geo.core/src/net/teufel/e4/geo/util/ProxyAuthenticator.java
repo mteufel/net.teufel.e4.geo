03e7eee73614b91bef1c93bffe37dcbc989ae100
mteufel <marc.teufel@hama.de>
2012-05-12 11:49:58 +0200

    ProxyAuthenticator Implementation

---
+++ net.teufel.e4.geo.core/src/net/teufel/e4/geo/util/ProxyAuthenticator.java
@@ -0,0 +1,22 @@
+package net.teufel.e4.geo.util;
+
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+
+public class ProxyAuthenticator {
+
+	private final static String PROXY_HOST = "???";
+	private final static String PROXY_PORT = "???";
+	private final static String PROXY_USER = "???";
+	private final static String PROXY_PASS = "???";
+
+	public static void authenticate() {
+		System.setProperty("http.proxyHost",PROXY_HOST) ;
+		System.setProperty("http.proxyPort", PROXY_PORT) ;
+		Authenticator.setDefault(new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(PROXY_USER,PROXY_PASS.toCharArray());
+			}
+		});
+	}
+}
